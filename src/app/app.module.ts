b120e9e10a64460befae32389afded62ddc7d766
Juancronos <juandavid.torres@gmail.com>
2018-04-21 16:54:10 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlTjW2ak59O76Po8lLb7hgdV6DBGAJOY8",
+  authDomain: "saludencasa-2018.firebaseapp.com",
+  databaseURL: "https://saludencasa-2018.firebaseio.com",
+  projectId: "saludencasa-2018",
+  storageBucket: "saludencasa-2018.appspot.com",
+  messagingSenderId: "90021284431"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: Observable<any[]>; 
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public db: AngularFirestore) {
+    this.items = db.collection('Productos').valueChanges();
   }
 
 }
